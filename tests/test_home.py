e2f11d3151daa85aed7e3e530ec2d766b8e8a836
markuusche <markuusche@gmail.com>
2025-09-09 15:24:04 +0800

    sample changes

--- tests/test_home.py
+++ tests/test_home.py
@@ -1,9 +1,4 @@
-from src.main import Tableau
+"""
+THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG!
 
-class Test:
-
-    data = Tableau()
-
-    def test_report(self, driver):
-        self.data.gameReport(driver, page=True)
-        self.data.gameData()
+"""
